643900c74a610ad74a6572e69e72ab98ca957d08
baduyifei <baduyifei@qq.com>
2024-12-24 17:21:08 +0800

    Refactor App.vue: remove checkbox selection functionality and simplify component structure by eliminating unused data and methods.

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,9 @@
 <!-- @format -->
 
-<template>
-	<div>
-		<span>全选:</span>
-		<input type="checkbox" v-model="bigSelectBtn" @change="bigSelect" />
-		<button @click="invertSelection">反选</button>
-		<ul>
-			<li v-for="(item, index) in arr" :key="index">
-				<input type="checkbox" v-model="item.c" @change="smallSelect" />
-				<span>{{ item.name }}</span>
-			</li>
-		</ul>
-	</div>
-</template>
+<template></template>
 
 <script>
-	export default {
-		data() {
-			return {
-				arr: [
-					{
-						name: '猪八戒',
-						c: false,
-					},
-					{
-						name: '孙悟空',
-						c: false,
-					},
-					{
-						name: '唐僧',
-						c: false,
-					},
-					{
-						name: '白龙马',
-						c: false,
-					},
-				],
-				bigSelectBtn: false,
-			};
-		},
-		methods: {
-			// 1. 小选 => 控制大选
-			smallSelect() {
-				// this.bigSelectBtn = this.arr.every((item) => item.c === true);
-				this.bigSelectBtn = this.arr.every((item) => item.c === true); // 更新全选按钮状态
-			},
-			// 2. 大选 => 控制所有小选
-			bigSelect() {
-				this.arr.forEach((item) => (item.c = this.bigSelectBtn));
-			},
-			// 3. 反选按钮
-			invertSelection() {
-				// 方法 1:
-				// this.bigSelectBtn = !this.bigSelectBtn;
-				// this.bigSelect(); // 调用方法, 更新4 个小选按钮状态
-				// 方法 2:
-				this.arr.forEach((item) => {
-					item.c = !item.c;
-				});
-				this.smallSelect(); // 调用方法, 更新全选按钮状态
-			},
-		},
-	};
+	export default {};
 </script>
 
 <style></style>
