3334c35b076d5541e60c34312fc9f207d0d9b9a3
baduyifei <baduyifei@qq.com>
2024-12-25 16:03:40 +0800

    Add score management component: create ScoreCase.vue for displaying and managing scores with a table layout, including functionality to add and delete scores, and format exam dates.

---
+++ src/10-App copy.vue
@@ -0,0 +1,42 @@
+<!-- @format -->
+
+<template>
+	<div>
+		<ul>
+			<li v-for="(item, i) in list" :key="i">{{ item }}</li>
+		</ul>
+		<h2>总分(方法调用): {{ getSum() }}</h2>
+		<h2>总分(方法调用): {{ getSum() }}</h2>
+		<h2>总分(方法调用): {{ getSum() }}</h2>
+		<h2>总分2(计算属性): {{ getSum2 }}</h2>
+		<h2>总分2(计算属性): {{ getSum2 }}</h2>
+		<h2>总分2(计算属性): {{ getSum2 }}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [88, 99, 100],
+			};
+		},
+		methods: {
+			getSum() {
+				console.log('方法调用了');
+				return this.list.reduce((sum, item) => (sum += item), 0);
+
+				//
+			},
+		},
+		computed: {
+			getSum2() {
+				console.log('计算属性调用了');
+				return this.list.reduce((sum, item) => (sum += item), 0);
+				//
+			},
+		},
+	};
+</script>
+
+<style></style>
